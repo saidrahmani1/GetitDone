6a2179d12c611e12c67425ce98b61202beb37aeb
A Rahmani <arzurahmani@arzus-macbook-pro.local>
2019-02-01 14:44:25 +0000

    Initial Commit

---
+++ GetitDone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetitDone
+//
+//  Created by A Rahmani on 01/02/2019.
+//  Copyright © 2019 Said Rahmani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
